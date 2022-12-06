6e6be8ac502f42bcc380277b46b497b17aeda70c
Jamie Taber-Plaisted <jamietp113@gmail.com>
2022-12-05 20:03:36 -0500

    Final Commit

--- week-three/day-3-HW.js
+++ week-three/day-3-HW.js
@@ -5,48 +5,56 @@ class Person {
         this.personalityType = personalityType;
     }
     sayHello() {
-        console.log(`${this.name} says hello!`)
+        return `${this.name} says hello!`
     }
     hair() {
-        console.log(`${this.name}'s hair color is ${this.hairColor}`)
+        return `${this.name}'s hair color is ${this.hairColor}`
     }
     personality(){
-        console.log(`${this.name}'s personality is ${this.personalityType}`)
+        return `${this.name}'s personality is ${this.personalityType}`
     }
 }
 
 class Governor extends Person{
-    constructor(canMakeLaws, yearsServed) {
+    static district = 'Texas';
+    static party = 'independent';
+    constructor(hairColor, name, personalityType, canMakeLaws, yearsServed) {
+        super(hairColor, name, personalityType);
         this.canMakeLaws = canMakeLaws;
         this.yearsServed = yearsServed;
     }
     service() {
-        console.log(`This Governor has served ${this.yearsServed} years`)
+        return `This Governor has served ${this.yearsServed} years`
     }
     laws() {
         if (this.canMakeLaws === true) {
-            console.log('This Governor can make laws')
+            return 'This Governor can make laws'
         } else {
-            console.log('This Governor cannot make laws')
+            return 'This Governor cannot make laws'
         }
     }
 }
+let Governor1 = new Governor('white', 'Benny', 'Sketchy', true, 12)
+console.log(Governor.district, Governor.party, Governor1)
 
 class PostalWorker extends Person{
     deliver(){
-        console.log(`${this.name} has delivered your mail`)
+        return `${this.name} has delivered your mail`
     }
     runFromDog(){
-        console.log('AHHHH PLEASE GET YOUR DOG BEFORE HE GETS ME!!!')
+        return 'AHHHH PLEASE GET YOUR DOG BEFORE HE GETS ME!!!'
     }
 }
 
 class Chef extends Person{
+    constructor(hairColor, name, personalityType) {
+        super(hairColor, name, personalityType);
+    }
     cooks(){
-        console.log(`${this.name} has prepared a wonderful meal`)
+        return `${this.name} has prepared a wonderful meal`
     }
     cleans(){
-        console.log(`${this.name} cleans up their mess`)
+        return `${this.name} cleans up their mess`
     }
 }
 
@@ -55,7 +63,7 @@ let Chef2 = new Chef('brown', 'Tom', 'Loud')
 let PostalWorker1 = new PostalWorker('bald', 'Terry', 'Scary')
 let PostalWorker2 = new PostalWorker('green', 'Casandra', 'Outgoing')
 
-console.log(Chef1.cooks(1), Chef1.cleans())
-console.log(Chef2.cooks(), Chef2.cleans())
-console.log(PostalWorker1.deliver(), PostalWorker1.runFromDog())
-console.log(PostalWorker2.deliver(), PostalWorker2.runFromDog())
+console.log(Chef1.cooks(), Chef1.cleans(), Chef1)
+console.log(Chef2.cooks(), Chef2.cleans(), Chef2)
+console.log(PostalWorker1.deliver(), PostalWorker1.runFromDog(), PostalWorker1)
+console.log(PostalWorker2.deliver(), PostalWorker2.runFromDog(), PostalWorker2)
