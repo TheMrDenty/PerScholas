63a952df345a932ad9d0616b2d368d8b1c43eada
Jamie Taber-Plaisted <jamietp113@gmail.com>
2022-12-07 09:29:08 -0500

    lab completion week4-day1

---
+++ week-four/day-1.js
@@ -0,0 +1,29 @@
+/* 
+// Task 1
+
+function minusOne(arg) {
+    return arg - 1;
+}
+
+console.log(minusOne(100), minusOne(50), minusOne(1))
+ */
+
+/* 
+// Task 2
+
+function makeSentence(who, verb, food){
+    return `Did you ask ${who} if they ${verb} some ${food}?`
+}
+
+console.log(makeSentence('them', 'want', 'chimichangas')); 
+*/
+
+/* 
+// Task 3
+
+function getLastElement(arr){
+    return arr[arr.length - 1]
+}
+
+console.log(getLastElement([2, 4, 6, 8, 200]))
+ */
