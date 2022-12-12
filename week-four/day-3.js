78af60f3c3aec368ef3bf7363e765bb5a24f3366
Jamie Taber-Plaisted <jamietp113@gmail.com>
2022-12-12 09:35:30 -0500

    Fizz Buzz answered

--- week-four/day-3.js
+++ week-four/day-3.js
@@ -4,6 +4,17 @@
     console.log(i);
 } */
 
-for (let i=0; i<=200; i+=2){
+/* for (let i=0; i<=200; i+=2){
+    console.log(i);
+} */
+
+for (let i = 0; i <= 100; i++){
+    if(i % 3 === 0 && i % 5 === 0) {
+        console.log(`${i} FizzBuzz`);
+    } else if (i % 3 === 0) {
+        console.log(`${i} Fizz`);
+    } else if (i % 5 === 0) {
+        console.log(`${i} Buzz`);
+    }
     console.log(i);
 }
