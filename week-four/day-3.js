9b33775ab39e562aa7c7b3f5327420eae367074f
Jamie Taber-Plaisted <jamietp113@gmail.com>
2022-12-12 09:39:45 -0500

    Wild Wild Life answered

--- week-four/day-3.js
+++ week-four/day-3.js
@@ -8,7 +8,7 @@
     console.log(i);
 } */
 
-for (let i = 0; i <= 100; i++){
+/* for (let i = 0; i <= 100; i++){
     if(i % 3 === 0 && i % 5 === 0) {
         console.log(`${i} FizzBuzz`);
     } else if (i % 3 === 0) {
@@ -17,4 +17,19 @@ for (let i = 0; i <= 100; i++){
         console.log(`${i} Buzz`);
     }
     console.log(i);
-}
+} */
+
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+
+plantee[2] += 1;
+console.log(plantee);
+wolfy[3] = 'Gotham City'
+console.log(wolfy);
+dart[3] = ["Upside Down", "Hawkins"]
+console.log(dart);
+wolfy[0] = 'Gameboy'
+console.log(wolfy);
