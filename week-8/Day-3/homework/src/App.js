9063b0465957466e0b2856478a168ee10d1020ac
Jamie Taber-Plaisted <jamietp113@gmail.com>
2023-01-27 17:51:43 -0500

    styling added

--- week-8/Day-3/homework/src/App.js
+++ week-8/Day-3/homework/src/App.js
@@ -1,8 +1,8 @@
 import Student from "./components/Student";
-
+import './style.css'
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Student />
     </div>
   );
--- week-8/Day-3/homework/src/components/Student.js
+++ week-8/Day-3/homework/src/components/Student.js
@@ -2,30 +2,41 @@ import React from 'react'
 import students from '../data'
 function Student() {
   return (
-    <div>
-        {
-            students.map((student) => {
-            const {name, bio, scores} = student;
-            console.log(scores);
-            return (
-                <div className='student'>
-                    <h1>{name}</h1>
-                    <p>{bio}</p>
-                    <ul>
-                        {scores.map(({date, score}) =>{
-                            return(
-                                <li>
-                                    <h1>Grade: {score}</h1>
-                                    <date>{date}</date>
-                                </li>
-                            )
-                        })}
-                    </ul>
-                </div>
-            );
-            })
-        }
-    </div>
+    <>
+        <nav>
+            <h1>Class Grades</h1>
+        </nav>
+        <div className='container'>
+            
+            {
+                students.map((student) => {
+                const {name, bio, scores} = student;
+                console.log(scores);
+                return (
+                    <div className='student'>
+                        <h1>{name}</h1>
+                        <p>{bio}</p>
+                        <ul>
+                            {scores.map(({date, score}) =>{
+                                return(
+                                    <li>
+                                        <div className='score'>
+                                            <h2>Grade: {score}</h2>
+                                            <p>~ {date}</p>
+                                        </div>
+                                    </li>
+                                )
+                            })}
+                        </ul>
+                    </div>
+                );
+                })
+            }
+        </div>
+        <footer>
+            
+        </footer>
+    </>
   )
 }
 
